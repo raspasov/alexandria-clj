f02499202b9a408406eaa9c93c725601a7d86bba
Rangel Spasov <raspasov@gmail.com>
2023-10-01 18:32:10 -0700

    repl-debug

---
+++ src/ss/clj/repl_debug.clj
@@ -0,0 +1,25 @@
+(ns ss.clj.repl-debug
+  "A simple REPL debugger. (break) stops the code execution at any point and allows inspection of current locals.")
+
+(defn contextual-eval [ctx expr]
+  (eval
+    `(let [~@(mapcat (fn [[k v]] [k `'~v]) ctx)]
+       ~expr)))
+
+(defmacro local-context []
+  (let [symbols (keys &env)]
+    (zipmap (map (fn [sym] `(quote ~sym))
+              symbols)
+      symbols)))
+
+(defn readr [prompt exit-code]
+  (let [input (clojure.main/repl-read prompt exit-code)]
+    (if (= input ::tl)
+      exit-code
+      input)))
+
+(defmacro break []
+  `(clojure.main/repl
+     :prompt #(print "repl-debug=> ")
+     :read readr
+     :eval (partial contextual-eval (local-context))))
